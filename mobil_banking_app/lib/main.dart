2425668c7d4eceea85caaffd6666fd6013ef1b9b
Nick <nikolay.sigov@gmail.com>
2022-06-21 14:03:02 +0300

    card wallet app

--- mobil_banking_app/lib/main.dart
+++ mobil_banking_app/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:mobil_banking_app/pages/sign_in_page/sign_in_page.dart';
-import 'package:mobil_banking_app/pages/sign_up_page/sign_up_page.dart';
+import 'package:mobil_banking_app/pages/auth_pages/sign_in_page/sign_in_page.dart';
+import 'package:mobil_banking_app/pages/auth_pages/sign_up_page/sign_up_page.dart';
 import 'package:mobil_banking_app/pages/welcome_page/welcome_page.dart';
 
 void main() {
   runApp(
      MaterialApp(
-      home: SignUpPage(),
+      home: SignInPage(),
     ),
   );
 }
